16015be9546b1a63881612fa0bfa832af9266456
fadimata tillman <fadi.tillman@gmail.com>
2020-01-22 17:53:39 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,1 @@
-# Build your say_hello method here
-
-def say_hello(name)
-  puts "Hello #{name}!"
-end
-
-def greeting(name = "Ruby programmer!")
-  puts "Hello #{name}"
-  say_hello
-end
+# Build your say_hello method 
